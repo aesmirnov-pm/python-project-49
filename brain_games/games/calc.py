e831956a79b4cbfa59df6bb7b43a36a219764af0
artem <artem_smirnov88@mail.ru>
2024-10-19 17:53:28 +0300

    fix error 17.10.24

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,16 +3,20 @@
 import brain_games.engine as engine
 import random
 from brain_games.utils import get_random_number
+from operator import add, sub, mul
 
 INSTRUCTION = 'What is the result of the expression?'
-LIST_OPERATOR = ['+', '-', '*']
+OPERATOR = {
+    '+': add,
+    '*': mul,
+    '-': sub,
+}
 
 
 def return_question_and_answer():
     number_1 = get_random_number()
     number_2 = get_random_number()
-    operator = random.choice(LIST_OPERATOR)
+    operator = random.choice(list(OPERATOR.keys()))
+    correct_answer = OPERATOR[operator](number_1, number_2)
     question = engine.question(number_1, operator, number_2)
-    expression = f"{number_1}{operator}{number_2}"
-    correct_answer = eval(expression)
-    return question, correct_answer
+    return question, str(correct_answer)
